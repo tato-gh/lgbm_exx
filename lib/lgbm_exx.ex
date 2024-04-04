d6b97f9c63eff325daa022a701d5404142b52f39
tato <61908291+tato-gh@users.noreply.github.com>
2024-04-04 22:55:02 +0900

    feat: mix new

---
+++ lib/lgbm_exx.ex
@@ -0,0 +1,18 @@
+defmodule LgbmExx do
+  @moduledoc """
+  Documentation for `LgbmExx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LgbmExx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lgbm_exx_test.exs
@@ -0,0 +1,8 @@
+defmodule LgbmExxTest do
+  use ExUnit.Case
+  doctest LgbmExx
+
+  test "greets the world" do
+    assert LgbmExx.hello() == :world
+  end
+end
